4d3cfdb14ea5844f0e28fff4f9b89c6789d9e03c
Ahmedolly <yusuffhammed43@gmail.com>
2022-09-18 10:58:05 +0100

    designed the menu icon

--- about.js
+++ about.js
@@ -1,7 +1,22 @@
-let hamburger = document.querySelector('.hamburger-icon')
-    let mobileMenu = document.querySelector('.mobile-menu') 
+// let hamburger = document.querySelector('.hamburger-icon')
+//     let mobileMenu = document.querySelector('.mobile-menu') 
     
-    hamburger.addEventListener ('click', () => {
-        mobileMenu.classList.toggle('mobile-menu')
-        hamburger.classList.toggle('open').style.width = "100%";
-    }) 
+//     hamburger.addEventListener ('click', () => {
+//         mobileMenu.classList.toggle('mobile-menu')
+//         hamburger.classList.toggle('open').style.width = "100%";
+//     }) 
+ let menuBtn= document.getElementById('menu-btn')
+    let sideNav= document.getElementById('sidenav')
+    let menu= document.getElementById('menu')
+
+     sideNav.style.right == "-250px";
+
+    menuBtn.onclick= function (){
+        if(sideNav.style.right == '-250px'){
+             sideNav.style.right= '0';
+                menu.src= "homepage_asset/closeicon.png"
+        } else{
+              sideNav.style.right = '-250px';
+               menu.src= "homepage_asset/menu.png"
+        }
+    }
